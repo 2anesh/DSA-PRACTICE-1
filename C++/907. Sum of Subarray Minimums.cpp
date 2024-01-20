b79abd7c61de642171d4f7e2091ded91b02ffb57
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-01-20 19:13:03 +0530

    Create 907. Sum of Subarray Minimums.cpp

---
+++ C++/907. Sum of Subarray Minimums.cpp
@@ -0,0 +1,26 @@
+class Solution {
+ public:
+  int sumSubarrayMins(vector<int>& arr) {
+    constexpr int kMod = 1'000'000'007;
+    const int n = arr.size();
+    long res = 0;
+    vector<int> prev(n, -1);
+    vector<int> next(n, n);
+    stack<int> stack;
+    for (int i = 0; i < n; ++i) {
+      while (!stack.empty() && arr[stack.top()] > arr[i]) {
+        const int index = stack.top();
+        stack.pop();
+        next[index] = i;
+      }
+      if (!stack.empty())
+        prev[i] = stack.top();
+      stack.push(i);
+    }
+    for (int i = 0; i < n; ++i) {
+      res += static_cast<long>(arr[i]) * (i - prev[i]) * (next[i] - i);
+      res %= kMod;
+    }
+    return res;
+  }
+};
