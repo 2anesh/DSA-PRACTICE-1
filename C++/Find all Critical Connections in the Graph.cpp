38cbd35deff8843e7e89bc0a6c3b729fa674f4f3
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-02-14 13:34:07 +0530

    Create Find all Critical Connections in the Graph.cpp

---
+++ C++/Find all Critical Connections in the Graph.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dfs(vector<int> graph[], int i, vector<int>& rank, vector<vector<int>>& res, int prev_rank, int n) {
+        rank[i] = prev_rank + 1;
+        int t = INT_MAX;
+        for(const auto& nbr: graph[i]) {
+            if(rank[nbr] == -1) {
+                int k = dfs(graph, nbr, rank, res, prev_rank+1, n);
+                if( k > rank[i])
+                    res.push_back({min(i, nbr), max(i,nbr)});
+                t = min(t, k);
+                
+            } else if(rank[nbr] == n+1)
+                continue;
+            else if(rank[nbr] != prev_rank)
+                t = min(t, rank[nbr]);
+        }
+        rank[i] = n + 1;
+        return t;
+    }
+	vector<vector<int>>criticalConnections(int v, vector<int> adj[]){
+	    vector<vector<int>> res;
+	    vector<int> rank(v, -1);
+	    int k = dfs(adj, 0, rank, res, 0, v);
+	    sort(res.begin(), res.end());
+	    return res;
+	}
+};
