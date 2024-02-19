37f897c7a8b5382bb4e4e376263f9636179073c0
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-02-19 17:03:18 +0530

    Create Game with String.java

---
+++ JAVA/Game with String.java
@@ -0,0 +1,25 @@
+class Solution{
+    static int minValue(String s, int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int arr[] = new int[26];
+        for(char c : s.toCharArray()){
+            arr[c - 'a']++;
+        }
+        for(int it : arr){
+            if(it > 0) {
+                pq.offer(it);
+            }
+        }
+        while(k > 0){
+            int num = pq.poll();
+            num -= 1;
+            pq.offer(num);
+            k--;
+        }
+        int ans = 0;
+        for(int n : pq){
+            ans += (n*n);
+        }
+        return ans;
+    }
+}
