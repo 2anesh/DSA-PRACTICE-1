3806bf901fbbc97e80d7215ae0875bd2a92be19c
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-02-22 15:45:00 +0530

    Create Distinct occurrences.cpp

---
+++ C++/Distinct occurrences.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+    int subsequenceCount(string s, string t)
+    {
+    int n=s.length(),m=t.length();
+      int mod=1e9+7;
+      vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+      for(int i=0;i<n;i++)dp[i][0]=1;
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+              if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j-1];
+              dp[i][j]=(dp[i][j]+dp[i-1][j])%mod;
+          }
+      }
+      return dp[n][m];
+    }
+};
