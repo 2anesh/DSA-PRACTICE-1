0b20c76aea568b9cf1891bad61a72d5690becfcd
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-02-26 23:38:00 +0530

    Create Power Set.java

---
+++ JAVA/Power Set.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public List<String> AllPossibleStrings(String s)
+    {
+        // Code here
+        List<String> list = new ArrayList<>();
+        list.add("");
+        for (char ch : s.toCharArray()) {
+            int n = list.size();
+            for (int i = 0; i < n; i++) {
+                String str = list.get(i);
+                StringBuilder sb = new StringBuilder(str);
+                sb.append(ch);
+                list.add(sb.toString());
+            }
+        }
+        list.remove(0);
+        Collections.sort(list);
+        return list;
+    }
+}
