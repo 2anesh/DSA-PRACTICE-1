70dcaea1e2208a6659de69e2d081cead3db1de99
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-02-29 17:19:07 +0530

    Create 1609. Even Odd Tree.java

---
+++ JAVA/1609. Even Odd Tree.java
@@ -0,0 +1,22 @@
+class Solution {
+  public boolean isEvenOddTree(TreeNode root) {
+    Queue<TreeNode> q = new LinkedList<>(Arrays.asList(root));
+    boolean isEven = true;
+    for (; !q.isEmpty(); isEven = !isEven) {
+      int prevVal = isEven ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+      for (int sz = q.size(); sz > 0; --sz) {
+        TreeNode node = q.poll();
+        if (isEven && (node.val % 2 == 0 || node.val <= prevVal))
+          return false; 
+        if (!isEven && (node.val % 2 == 1 || node.val >= prevVal))
+          return false; 
+        prevVal = node.val;
+        if (node.left != null)
+          q.offer(node.left);
+        if (node.right != null)
+          q.offer(node.right);
+      }
+    }
+    return true;
+  }
+}
