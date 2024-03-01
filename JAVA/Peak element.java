6c238f872cfe80211c07b95b8eae1782fb54ddc0
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-03-01 11:41:24 +0530

    Create Peak element.java

---
+++ JAVA/Peak element.java
@@ -0,0 +1,18 @@
+class Solution
+{
+	public int peakElement(int[] arr,int n)
+    {
+       int i = 0, j = n-1;
+       int peak = 0;
+      while(i<j){
+          if(arr[i]<arr[j]){
+              peak = j;
+              i++;
+          } else if(arr[i]>=arr[j]){
+              peak = i;
+              j--;
+          }
+      }
+       return peak;
+    }
+}
