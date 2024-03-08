e8c265b8a8b31a9485a3bfca5e86c00a5acf409b
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-03-08 13:47:44 +0530

    Create 3005. Count Elements With Maximum Frequency.java

---
+++ JAVA/3005. Count Elements With Maximum Frequency.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {        
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int e : nums) {
+            freq.put(e, freq.getOrDefault(e, 0) + 1);
+        }
+        int maxFreq = 0;
+        for(int e : freq.values()) {
+            maxFreq = Math.max(maxFreq, e);
+        }
+        int freqOfMaxFrequency = 0;
+        for(int frequency : freq.values()) {
+            if(frequency == maxFreq) {
+                freqOfMaxFrequency++;
+            }
+        }
+        return freqOfMaxFrequency * maxFreq;
+    }
+}
