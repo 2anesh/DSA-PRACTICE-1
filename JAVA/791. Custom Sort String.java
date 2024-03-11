ae7346334ecaf350018ac98f44f5ae344dfd6797
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-03-11 20:09:55 +0530

    Create 791. Custom Sort String.java

---
+++ JAVA/791. Custom Sort String.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String customSortString(String order, String str) {        
+        int alpha[] = new int[26];
+        for(int i = 0; i<str.length();i++) alpha[str.charAt(i)-97]++;
+        String ans = "";
+        for(int i =0; i<order.length();i++){
+            ans += (""+order.charAt(i)).repeat(alpha[order.charAt(i)-97]);
+            alpha[order.charAt(i)-97]=0;            
+        }
+        for(int i=0;i<26;i++){
+            ans += (""+(char)(i+97)).repeat(alpha[i]);
+        }
+        return ans;
+    }
+}
