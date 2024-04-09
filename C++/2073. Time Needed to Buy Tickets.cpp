914b765b660f1f2e9ff4f248c248fb5e76f55d43
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-04-09 21:18:21 +0530

    Create 2073. Time Needed to Buy Tickets.cpp

---
+++ C++/2073. Time Needed to Buy Tickets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int cnt=0;
+        while(1){
+            for(int i=0;i<tickets.size();i++){
+                if(tickets[i]==0) 
+                    continue;
+                tickets[i]--; 
+                cnt++;
+                if(tickets[k]==0) 
+                return cnt;
+            }
+        }
+        }
+};
