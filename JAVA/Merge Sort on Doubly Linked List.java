c89945b15874c2f45eb98514a4419e48e4765d79
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-04-27 12:50:15 +0530

    Create Merge Sort on Doubly Linked List.java

---
+++ JAVA/Merge Sort on Doubly Linked List.java
@@ -0,0 +1,54 @@
+class Solution {
+    static Node sortDoubly(Node head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node mid = getMid(head);
+        Node l1 = sortDoubly(head);
+        Node l2 = sortDoubly(mid);
+        return merge(l1,l2);
+    }
+    static Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow.next;
+        slow.next = null;
+        mid.prev = null;
+        return mid;
+    }
+    static Node merge(Node l1, Node l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        Node head = new Node(-1);
+        Node tail = head;
+        while(l1 != null && l2 != null){
+            if(l1.data < l2.data){
+                l1.prev = tail;
+                tail.next = l1;
+                l1 = l1.next;
+                tail = tail.next;
+            }
+            else{
+                l2.prev = tail;
+                tail.next = l2;
+                l2 = l2.next;
+                tail = tail.next;
+            }
+        }
+        if(l1 != null){
+            l1.prev = tail;
+            tail.next = l1;
+        }
+        if(l2 != null){
+            l2.prev = tail;
+            tail.next = l2;
+        }
+        head = head.next;
+        head.prev = null;
+        return head;
+    }
+}
