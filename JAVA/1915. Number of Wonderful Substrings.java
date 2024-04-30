22b4f849895685e18792fb3b1c49499e81965c85
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-04-30 15:04:53 +0530

    Create 1915. Number of Wonderful Substrings.java

---
+++ JAVA/1915. Number of Wonderful Substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long wonderfulSubstrings(String word) {
+        long result = 0;
+        int[] count = new int[1024];
+        count[0] = 1;
+        int mask = 0;
+
+        for (char c : word.toCharArray()) {
+            mask ^= 1 << (c - 'a');
+            result += count[mask];
+            for (int i = 0; i < 10; i++) {
+                result += count[mask ^ (1 << i)];
+            }
+            count[mask]++;
+        }
+
+        return result;
+    }
+}
