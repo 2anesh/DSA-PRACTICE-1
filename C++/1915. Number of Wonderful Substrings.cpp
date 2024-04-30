ecbdbe4a9331f5b546a6c0306aeeba20b73f364d
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-04-30 14:57:53 +0530

    Create 1915. Number of Wonderful Substrings.cpp

---
+++ C++/1915. Number of Wonderful Substrings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  long long wonderfulSubstrings(string word) {
+    long long ans = 0;
+    int prefix = 0;           
+    vector<int> count(1024); 
+    count[0] = 1;             
+    for (const char c : word) {
+      prefix ^= 1 << c - 'a';
+      ans += count[prefix];
+      for (int i = 0; i < 10; ++i)
+        ans += count[prefix ^ 1 << i];
+      ++count[prefix];
+    }
+    return ans;
+  }
+};
