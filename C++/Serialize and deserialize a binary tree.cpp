0ee7ca3755de7f3df84ab1c74bb1cdc0a63135de
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-05-02 21:30:52 +0530

    Create Serialize and deserialize a binary tree.cpp

---
+++ C++/Serialize and deserialize a binary tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void inorder(Node* root, vector<int>& v) {
+        if (root == nullptr) return;
+        inorder(root->left, v);
+        v.push_back(root->data);
+        inorder(root->right, v);
+    }
+    vector<int> serialize(Node *root) {
+        vector<int> v;
+        inorder(root, v);
+        return v;
+    }
+    Node* buildtree(vector<int>& v, vector<int>::iterator start, vector<int>::iterator end) {
+        if (start == end) return nullptr;
+        auto mid = start + (end - start) / 2;
+        Node* root = new Node(*mid);
+        root->left = buildtree(v, start, mid);
+        root->right = buildtree(v, mid + 1, end);
+        return root;
+    }
+    Node* deSerialize(vector<int>& v) {
+        return buildtree(v, v.begin(), v.end());
+    }
+};
