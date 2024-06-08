d9b6eb03a4857b2d761c2977654d757c05c5af34
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-06-08 16:31:25 +0530

    Create Index of an Extra Element.java

---
+++ JAVA/Index of an Extra Element.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findExtra(int n, int arr1[], int arr2[]) {
+        for(int i=0;i<n-1;i++){
+            if(arr1[i]!=arr2[i]){
+                return i;
+            }
+        }
+        return n-1;
+    }
+}
