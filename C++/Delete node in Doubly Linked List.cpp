3ae74d0fe982cde6a120e45b2b112f61c4acc901
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-06-30 19:14:13 +0530

    Create Delete node in Doubly Linked List.cpp

---
+++ C++/Delete node in Doubly Linked List.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+  Node*findnode(Node*head,int x)
+  {
+      int count=1;
+      Node*temp=head;
+      while(count!=x)
+      {
+          temp=temp->next;
+          count++;
+      }
+      return temp->prev;
+  }
+    Node* deleteNode(Node* head, int x) {
+        if(x==1)return head->next;
+        Node*temp=findnode(head,x);
+        temp->next->prev=temp;
+        temp->next=temp->next->next;
+        return head;
+    }
+};
