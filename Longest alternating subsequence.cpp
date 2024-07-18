541e80f226b58d8409f0b3a1ca7c8be9d4c934c5
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-07-18 16:48:12 +0530

    Create Longest alternating subsequence.cpp

---
+++ Longest alternating subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int alternatingMaxLength(vector<int>& arr) {
+        int n = arr.size();
+    if (n == 0) return 0;
+    int up = 1, down = 1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[i - 1]) {
+            up = down + 1;
+        } 
+        else if (arr[i] < arr[i - 1]) {
+            down = up + 1;
+        }
+    }
+    return max(up, down); 
+    }
+};
