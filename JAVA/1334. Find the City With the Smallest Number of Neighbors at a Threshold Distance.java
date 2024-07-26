0ea3aadf11c4cdf89046298284f2df1ceb54667f
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-07-26 12:23:56 +0530

    Create 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.java

---
+++ JAVA/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
+            }
+            dist[i][i] = 0;
+        }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            dist[u][v] = w;
+            dist[v][u] = w;
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                if (dist[i][k] != Integer.MAX_VALUE) {
+                    for (int j = 0; j < n; j++) {
+                        if (dist[k][j] != Integer.MAX_VALUE) {
+                            dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                        }
+                    }
+                }
+            }
+        }
+        int cntCity = n;
+        int cityNo = -1;
+        for (int city = 0; city < n; city++) {
+            int cnt = 0;
+            for (int adjCity = 0; adjCity < n; adjCity++) {
+                if (dist[city][adjCity] <= distanceThreshold && city != adjCity) {
+                    cnt++;
+                }
+            }
+            if (cnt <= cntCity) {
+                cntCity = cnt;
+                cityNo = city;
+            }
+        }
+        return cityNo;
+    }
+}
