8356581ce9476a34bedd87a3ec0c464ab907e466
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-08-12 16:24:18 +0530

    Create 703. Kth Largest Element in a Stream.java

---
+++ JAVA/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,17 @@
+class KthLargest {    
+    PriorityQueue<Integer> q = new PriorityQueue<>(); 
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        for(int i:nums){
+            q.offer(i);
+        }
+        while(q.size()>k) q.poll();
+    }    
+    public int add(int val) {
+        q.add(val);
+        while(q.size()>k)
+        q.poll();
+        return q.peek();        
+    }
+}
