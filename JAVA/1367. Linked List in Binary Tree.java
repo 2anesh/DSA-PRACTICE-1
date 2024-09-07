d104d9b8beae7a270930441ae3622fdcc869cf21
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-09-07 15:39:23 +0530

    Create 1367. Linked List in Binary Tree.java

---
+++ JAVA/1367. Linked List in Binary Tree.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+    private boolean dfs(ListNode head, TreeNode node) {
+        if (head == null) {
+            return true;
+        }
+        if (node == null) {
+            return false;
+        }
+        if (head.val != node.val) {
+            return false;
+        }
+        return dfs(head.next, node.left) || dfs(head.next, node.right);
+    }
+}
