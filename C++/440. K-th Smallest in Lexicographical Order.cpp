6bdcf61fadb804fc98aed0718661848af6f096d1
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-09-22 13:18:51 +0530

    Create 440. K-th Smallest in Lexicographical Order.cpp

---
+++ C++/440. K-th Smallest in Lexicographical Order.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        int x = 1; 
+        while (k > 1) {
+            long cnt = 0; 
+            for (long diff = 1, curr = x; curr <= n; curr *= 10, diff *= 10) 
+                cnt += min(n - curr + 1, diff); 
+            if (k > cnt) k -= cnt, ++x; 
+            else --k, x *= 10; 
+        }
+        return x; 
+    }
+};
