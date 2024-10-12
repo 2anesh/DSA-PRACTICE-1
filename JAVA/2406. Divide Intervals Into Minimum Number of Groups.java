609e84f351161e635a75e5a859b0b82389200258
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-10-12 15:40:01 +0530

    Create 2406. Divide Intervals Into Minimum Number of Groups.java

---
+++ JAVA/2406. Divide Intervals Into Minimum Number of Groups.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minGroups(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();        
+        for (int[] interval : intervals) {
+            if (!pq.isEmpty() && pq.peek() < interval[0]) {
+                pq.poll();
+            }
+            pq.offer(interval[1]);
+        }
+        return pq.size();
+    }
+}
