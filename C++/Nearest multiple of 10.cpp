6dd63eff689898f4ba1010f43369fcbc7748937b
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-10-19 16:01:09 +0530

    Create Nearest multiple of 10.cpp

---
+++ C++/Nearest multiple of 10.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+    string roundToNearest(string str) {
+        int n = str.size();
+        int last_digit = str[n - 1] - '0';
+        if(last_digit > 5){
+            str[n - 1] = '0';
+            int carry = 1;
+            int i = n - 2;
+            while(i >= 0 && carry){
+                int cur_digit = (str[i] - '0') + carry;
+                carry = cur_digit / 10;
+                if(cur_digit > 9){
+                    str[i] = '0';
+                }
+                else {
+                    str[i] = cur_digit + '0';
+                    return str;
+                }
+                i--;
+            }
+            if(carry){
+                return "1" + str;
+            }
+            
+        }
+        str[n - 1] = '0';
+        return str;
+    }
+};
