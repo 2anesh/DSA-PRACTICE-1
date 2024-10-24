202f3ec9750f1e9dc2ab523ee803ca3f14c45266
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-10-24 14:47:20 +0530

    Create 951. Flip Equivalent Binary Trees.java

---
+++ JAVA/951. Flip Equivalent Binary Trees.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1==null && root2==null)
+        return true;
+        if(root1==null || root2==null)
+        return false;
+
+        if(root1.val==root2.val)
+        {
+            boolean withoutFlip = flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right);
+            boolean withFlip  = flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left);
+            return withoutFlip || withFlip;
+        }          
+        return false;
+    }
+}
