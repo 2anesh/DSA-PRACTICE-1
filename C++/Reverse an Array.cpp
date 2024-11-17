172e7b533bcc2d58c523f4c903391688d7f39e97
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-11-17 13:47:16 +0530

    Create Reverse an Array.cpp

---
+++ C++/Reverse an Array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        int left = 0, right = arr.size()-1;
+        while(left<right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+};
