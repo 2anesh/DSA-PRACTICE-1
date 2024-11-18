8aefff051538e52760736bd8d2dfd07f287deea9
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2024-11-18 14:02:45 +0530

    Create Rotate Array.cpp

---
+++ C++/Rotate Array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    void rotateArr(vector<int>& arr, int d) {
+        d%=arr.size(); 
+        reverse(arr.begin(),arr.begin()+d);
+        reverse(arr.begin()+d,arr.end());
+        reverse(arr.begin(),arr.end());
+    }
+};
