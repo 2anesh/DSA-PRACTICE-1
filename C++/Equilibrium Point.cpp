26d7a44c84dd2d858451b7d915cb219531f0025f
Ganesh kumar <98741308+2anesh@users.noreply.github.com>
2025-01-14 16:24:04 +0530

    Create Equilibrium Point.cpp

---
+++ C++/Equilibrium Point.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int findEquilibrium(vector<int> &arr) {
+        int n=arr.size();
+        vector<int>pre(n, 0), suf(n, 0);
+        
+        int sum=0;
+        for(int i=0; i<n; i++){
+            pre[i]=sum;
+            sum+=arr[i];
+        }
+        sum=0;
+        for(int i=n-1; i>=0; i--){
+            suf[i]=sum;
+            sum+=arr[i];
+        }
+        
+        for(int i=0; i<n; i++){
+            if(pre[i]==suf[i]) return i;
+        }
+        
+        return -1;
+    }
+};
